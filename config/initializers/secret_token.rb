4a0528a393ac53a8a5d7eebd65143a0c91af161b
SoonKhen OwYong <dude@owyong.sk>
2012-02-23 17:37:10 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GLAC::Application.config.secret_token = '99599896ed3d0c8031976237ed2951e97f6ac24eb5e25147f6945193072bb4e2136ebea67b833b35df9e847fd0eaed69b6e465848af7797aecaf71421191db5b'
