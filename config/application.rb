94a0e24f4e2b362d4964aef94f935f9b7d103850
SoonKhen OwYong <dude@owyong.sk>
2012-02-23 17:44:41 -0800

    added devise heroku in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module GLAC
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Devise requirement with Heroku, I know I know
+    config.assets.initialize_on_precompile = false
   end
 end
