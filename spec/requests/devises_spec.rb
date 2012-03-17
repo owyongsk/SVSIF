957ab4dfa01a78a9d43ff8340f0887c569cb9f02
SoonKhen OwYong <dude@owyong.sk>
2012-03-16 17:29:14 -0700

    merged tests with master

--- spec/requests/devises_spec.rb
+++ spec/requests/devises_spec.rb
@@ -4,7 +4,6 @@ describe "Devises" do
   describe "GET /signin" do
     it "should show a signin page" do
       visit signin_path
-      save_and_open_page
       page.should have_content("Email")
       page.should have_content("Password")
     end
