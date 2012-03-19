4ad004b2df3bdcdc43cd166e218a7be78e3e83f1
SoonKhen OwYong <dude@owyong.sk>
2012-03-19 00:15:42 -0700

    small mistakes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @title = "Welcome to SVIF"
+    @title = "Welcome to SVSIF"
     @profile = current_user.profile if user_signed_in?
   end
 end
